29ad5ef46aa0f967731f7870b030ef02f64bd400
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-11-06 14:39:18 -0500

    Remove sample entry comment

--- src/scraper.rs
+++ src/scraper.rs
@@ -36,20 +36,3 @@ pub async fn parse_sermon_links(page_data: String) -> Vec<SermonInfo> {
 
   sermons
 }
-
-/*
-  Sample Entry:
-
-  <div
-  class="sqs-audio-embed"
-  data-url="https://static1.squarespace.com/static/5ab2b6c9f7939229ff49a57c/t/634cad5f5f42f274256fceba/1665969570284/Lee+Bolton+10-16-22.mp3/original/Lee+Bolton+10-16-22.mp3"
-  data-mime-type=""
-  data-title="Faith Over Fear - Philippians 1:2-6"
-  data-author="Rev. Lee Bolton, 10-16-22"
-  data-show-download="false"
-  data-design-style="minimal"
-  data-duration-in-ms="1536000"
-  data-color-theme="dark"
->
-
-*/
