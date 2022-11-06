f46be9ed0ec903cb4963efc6a1f0b10edfc2b0b5
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-11-06 16:53:30 -0500

    Move scraping logic to web_scraper.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-mod scraper;
-
-use reqwest::{self, header};
-
-const SERMON_PAGE_URL: &str = "https://www.pinecrestbaptistcharleston.org/from-the-pulpit";
+mod web_scraper;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut headers = header::HeaderMap::new();
-    headers.insert(header::USER_AGENT,
-        header::HeaderValue::from_static("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"));
-
-
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()?;
-
-    let result = scraper::scrape_page(client, SERMON_PAGE_URL)
-        .await
-        .unwrap_or_else(|error| {
-            panic!("Oops! Something went wrong grabbing the site info: {:?}", error)
-        });
-
-    let sermons_found = scraper::parse_sermon_links(result).await;
+    let sermons_found = web_scraper::obtain_sermons().await.unwrap();
 
     println!("Here are the sermons that have been found:\n\n{:#?}", sermons_found);
 
--- src/web_scraper.rs
+++ src/web_scraper.rs
@@ -1,5 +1,8 @@
 use reqwest::Error;
 use scraper::{Html, Selector};
+use reqwest::{self, header};
+
+const SERMON_PAGE_URL: &str = "https://www.pinecrestbaptistcharleston.org/from-the-pulpit";
 
 #[derive(Debug)]
 pub struct SermonInfo {
@@ -8,13 +11,32 @@ pub struct SermonInfo {
   author: String
 }
 
-pub async fn scrape_page(client: reqwest::Client, url: &str) -> Result<String, Error> {
+pub async fn obtain_sermons() -> Result<Vec<SermonInfo>, Error> {
+  let mut headers = header::HeaderMap::new();
+  headers.insert(header::USER_AGENT,
+      header::HeaderValue::from_static("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"));
+
+
+  let client = reqwest::Client::builder()
+      .default_headers(headers)
+      .build()?;
+
+  let result = scrape_page(client, SERMON_PAGE_URL)
+      .await
+      .unwrap_or_else(|error| {
+          panic!("Oops! Something went wrong grabbing the site info: {:?}", error)
+      });
+
+  Ok(parse_sermon_links(result).await)
+}
+
+async fn scrape_page(client: reqwest::Client, url: &str) -> Result<String, Error> {
   let response = client.get(url).send().await?;
 
   response.text().await
 }
 
-pub async fn parse_sermon_links(page_data: String) -> Vec<SermonInfo> {
+async fn parse_sermon_links(page_data: String) -> Vec<SermonInfo> {
   let mut sermons = Vec::<SermonInfo>::new();
 
   let parsed_page = Html::parse_document(page_data.as_str());
