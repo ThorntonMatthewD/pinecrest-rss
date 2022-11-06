123dbe2a438ebc1f29cb6c8a5234d68d0ee6e9b5
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-11-06 14:11:48 -0500

    Add headers to request so I don't get pwned

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 mod scraper;
 
+use reqwest::{self, header};
+
 const SERMON_PAGE_URL: &str = "https://www.pinecrestbaptistcharleston.org/from-the-pulpit";
 
-fn main() {
-    scraper::scrape_page(SERMON_PAGE_URL)
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::USER_AGENT,
+        header::HeaderValue::from_static("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"));
+
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    let result = scraper::scrape_page(client, SERMON_PAGE_URL)
+        .await
         .unwrap_or_else(|error| {
             panic!("Oops! Something went wrong grabbing the site info: {:?}", error)
         });
+
+    println!("please work lol {result}");
+
+    Ok(())
 }
--- src/scraper.rs
+++ src/scraper.rs
@@ -1,7 +1,25 @@
 use reqwest::Error;
 
-pub fn scrape_page(url: &str) -> Result<String, Error> {
-  let response= reqwest::blocking::get(url)?;
 
-  response.text()
+pub async fn scrape_page(client: reqwest::Client, url: &str) -> Result<String, Error> {
+  let response = client.get(url).send().await?;
+
+  response.text().await
 }
+
+/*
+  Sample Entry:
+
+  <div
+  class="sqs-audio-embed"
+  data-url="https://static1.squarespace.com/static/5ab2b6c9f7939229ff49a57c/t/634cad5f5f42f274256fceba/1665969570284/Lee+Bolton+10-16-22.mp3/original/Lee+Bolton+10-16-22.mp3"
+  data-mime-type=""
+  data-title="Faith Over Fear - Philippians 1:2-6"
+  data-author="Rev. Lee Bolton, 10-16-22"
+  data-show-download="false"
+  data-design-style="minimal"
+  data-duration-in-ms="1536000"
+  data-color-theme="dark"
+>
+
+*/
